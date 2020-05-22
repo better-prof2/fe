139ede125c5ecc33a0fcbec556b698b5a39e1dba
bdrummo6 <bradley.drummonds@gmail.com>
2020-05-21 20:10:14 -0400

    Added dummy data, setup Redux store

---
+++ better-professor/src/dummy-data/messageData.js
@@ -0,0 +1,29 @@
+
+const messageData = [
+	{
+		id: 1,
+		recepient: 'Paquito Julia Cuéllar',
+		title: 'Your Project is Graded',
+		message: 'You made a 98 on your project.',
+		sendDate: '03/04/2021',
+		sendTime: '3:30 pm'
+	},
+	{
+		id: 2,
+		recepient: 'Meridith Nigella Rickard',
+		title: 'Welcome!',
+		message: 'Welcome to the class!',
+		sendDate: '03/09/2021',
+		sendTime: '3:50 pm'
+	},
+	{
+		id: 1,
+		recepient: 'Russell Eldred Taft',
+		title: 'Feedback on last project',
+		message: 'You did great on your last project!',
+		sendDate: '03/25/2021',
+		sendTime: '3:30 pm'
+	},
+]
+
+export default messageData;
--- better-professor/src/index.js
+++ better-professor/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux'; // importing Provider to wrap the App component in
+import { createStore } from 'redux'; // importing createStore to create a redux store
+import { reducer } from './reducers/reducer'; // importing the reducer to be an argument for the createStore function
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+       <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
