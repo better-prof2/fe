eafe1d24362551f2a86ee9aad156979010b0833a
Robert Gordon <Komeuppance@gmail.com>
2020-05-28 17:03:32 -0700

    Adding axios request in actions

--- better-professor/src/actions/actions.js
+++ better-professor/src/actions/actions.js
@@ -3,3 +3,8 @@ export const ADD_STUDENT = 'ADD_STUDENT';
 export const addStudent = student => dispatch => {
 	dispatch({type: ADD_STUDENT, payload: student})
 }
+
+const getData = () => dispatch => {
+	dispatch({type: DATA_LOAD_START});
+	
+}
